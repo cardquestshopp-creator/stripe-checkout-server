8d49e749e4416a83317d5377dffa332384299b4c
cardquestshopp-creator <cardquestshopp@gmail.com>
2026-02-05 19:33:28 -0600

    Update inventory.js

--- api/inventory.js
+++ api/inventory.js
@@ -3,8 +3,8 @@ import { google } from 'googleapis';
 const auth = new google.auth.JWT(
   process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
   null,
-  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  process.env.GOOGLE_PRIVATE_KEY, // Remove the .replace() since it already has real line breaks
+  ['https://www.googleapis.com/auth/spreadsheets']
 );
 
 const sheets = google.sheets({ version: 'v4', auth });
