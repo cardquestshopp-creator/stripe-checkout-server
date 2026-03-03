ff2dd74aed8ddc9854a5fa0b4816cb8870d87576
cardquestshopp-creator <cardquestshopp@gmail.com>
2026-03-02 20:12:06 -0600

    Update stripe-webhook.js

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -20,8 +20,8 @@ async function getRawBody(req) {
   return Buffer.concat(chunks);
 }
 
-export default async function handler(req, res.setHeader('Access) {
-  res-Control-Allow-Origin', '*');
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, stripe-signature');
 
